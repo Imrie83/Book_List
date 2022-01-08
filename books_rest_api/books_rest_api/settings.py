626d9b30d2f8a37a6347bea2d17d579cc8235976
Imrie83 <czolgista83@gmail.com>
2022-01-08 11:14:17 +0100

    add allowed hosts

--- books_rest_api/books_rest_api/settings.py
+++ books_rest_api/books_rest_api/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://my-book-list-api.herokuapp.com/',
+    'localhost',
+]
 
 
 # Application definition
