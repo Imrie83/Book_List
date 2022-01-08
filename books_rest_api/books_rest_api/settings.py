abc0ecc1aa38f23213e7afb7f0c346b814bbe20d
Imrie83 <czolgista83@gmail.com>
2022-01-08 11:52:15 +0100

    database test

--- books_rest_api/books_rest_api/settings.py
+++ books_rest_api/books_rest_api/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dj_database_url
+from django.conf.global_settings import DATABASES
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
